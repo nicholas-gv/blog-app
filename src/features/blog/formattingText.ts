164d487fa452c871e66fa905fe19598264e6cc6d
Nika870 <nikagvenetadze870@gmail.com>
2024-01-13 19:05:27 +0400

    refactored formattingText again

--- src/features/blog/formattingText.ts
+++ src/features/blog/formattingText.ts
@@ -8,17 +8,20 @@ export const wrapTextInHTMLTag = (
 ) => {
     const formats = {bold: 'b', italic: 'i', underline: 'ins', strikethrough: 'del', code: 'code'};
     const textarea = textareaElementRef.current;
-    const isntEmpty = textarea && textarea.selectionEnd !== textarea.selectionStart
+    const textNotEmpty = textarea && textarea.selectionEnd !== textarea.selectionStart
 
-    if (isntEmpty) {
+    if (textNotEmpty) {
         const textareaValue = textarea.value;
+        const precedingText = textareaValue.substring(0, textarea.selectionStart)
+        const succedingText = textareaValue.substring(textarea.selectionEnd, textareaValue.length)
+
         const newTextareaValue =
-            textareaValue.substring(0, textarea.selectionStart) +
+            precedingText +
             `<${formats[typeOfFormat]}>${textareaValue.substring(
                 textarea.selectionStart,
                 textarea.selectionEnd
             )}</${formats[typeOfFormat]}>` +
-            textareaValue.substring(textarea.selectionEnd, textareaValue.length);
+            succedingText;
         textareaElementRef.current.value = newTextareaValue;
     }
 };
