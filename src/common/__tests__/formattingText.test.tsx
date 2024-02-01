54932d7b8d39ee83740a012de4bb366867a32ed6
Nika870 <nikagvenetadze870@gmail.com>
2024-02-01 16:05:02 +0400

    changed formatting of test

--- src/common/__tests__/formattingText.test.tsx
+++ src/common/__tests__/formattingText.test.tsx
@@ -1,42 +1,48 @@
-import { wrapTextInHTMLTag } from '../../features/blog/formattingText';
-import { TextAreaRef } from '../../types/types';
-import { render, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import {wrapTextInHTMLTag} from '../../features/blog/formattingText';
+import {TextAreaRef} from '../../types/types';
+import {render, screen} from '@testing-library/react';
+import {act} from 'react-dom/test-utils';
 
 class MockRefObject<T> {
-  current: T | null = null;
+    current: T | null = null;
 }
 
 describe('wrapTextInHTMLTag', () => {
-  it('wraps selected text in HTML tags', () => {
-    const textareaRef: TextAreaRef = new MockRefObject<HTMLTextAreaElement>();
+    it('wraps selected text in HTML tags', () => {
+        const textareaRef: TextAreaRef = new MockRefObject<HTMLTextAreaElement>();
 
-    const { container } = render(
-      <textarea ref={textareaRef} defaultValue="Hello World" />
-    );
+        const {container} = render(
+            <textarea
+                ref={textareaRef}
+                defaultValue="Hello World"
+            />
+        );
 
-    const textarea = screen.getByRole('textbox') as HTMLTextAreaElement; 
+        const textarea = screen.getByRole('textbox') as HTMLTextAreaElement;
 
-    act(() => {
-      textarea.setSelectionRange(0, 5);
+        act(() => {
+            textarea.setSelectionRange(0, 5);
+        });
+
+        wrapTextInHTMLTag('bold', textareaRef);
+
+        expect(textarea.value).toBe('<b>Hello</b> World');
     });
 
-    wrapTextInHTMLTag('bold', textareaRef);
+    it('does not modify the text if no selection is made', () => {
+        const textareaRef: TextAreaRef = new MockRefObject<HTMLTextAreaElement>();
 
-    expect(textarea.value).toBe('<b>Hello</b> World');
-  });
-  
-  it('does not modify the text if no selection is made', () => {
-    const textareaRef: TextAreaRef = new MockRefObject<HTMLTextAreaElement>();
+        const {container} = render(
+            <textarea
+                ref={textareaRef}
+                defaultValue="Hello World"
+            />
+        );
 
-    const { container } = render(
-      <textarea ref={textareaRef} defaultValue="Hello World" />
-    );
+        const textarea = screen.getByRole('textbox') as HTMLTextAreaElement;
 
-    const textarea = screen.getByRole('textbox') as HTMLTextAreaElement; 
+        wrapTextInHTMLTag('bold', textareaRef);
 
-    wrapTextInHTMLTag('bold', textareaRef);
-
-    expect(textarea.value).toBe('Hello World');
-  });
+        expect(textarea.value).toBe('Hello World');
+    });
 });
